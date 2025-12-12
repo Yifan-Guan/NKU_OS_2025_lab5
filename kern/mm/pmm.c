1c1dcc93aeef27ec6fbecee5e438d96addbe44d1
yanqiuci <1943714609@qq.com>
2025-12-12 16:47:09 +0800

    Update pmm.c

--- kern/mm/pmm.c
+++ kern/mm/pmm.c
@@ -373,6 +373,15 @@ void exit_range(pde_t *pgdir, uintptr_t start, uintptr_t end)
  *
  * CALL GRAPH: copy_mm-->dup_mmap-->copy_range
  */
+/* copy_range - copy content of memory (start, end) of one process A to another
+ * process B
+ * @to:     the addr of process B's Page Directory
+ * @from:   the addr of process A's Page Directory
+ * @share: flags to indicate to dup OR share. We just use dup method, so it
+ * didn't be used.
+ *
+ * CALL GRAPH: copy_mm-->dup_mmap-->copy_range
+ */
 int copy_range(pde_t *to, pde_t *from, uintptr_t start, uintptr_t end,
                bool share)
 {
@@ -404,25 +413,17 @@ int copy_range(pde_t *to, pde_t *from, uintptr_t start, uintptr_t end,
             assert(page != NULL);
             assert(npage != NULL);
             int ret = 0;
-            /* LAB5:EXERCISE2 YOUR CODE
-             * replicate content of page to npage, build the map of phy addr of
-             * nage with the linear addr start
-             *
-             * Some Useful MACROs and DEFINEs, you can use them in below
-             * implementation.
-             * MACROs or Functions:
-             *    page2kva(struct Page *page): return the kernel vritual addr of
-             * memory which page managed (SEE pmm.h)
-             *    page_insert: build the map of phy addr of an Page with the
-             * linear addr la
-             *    memcpy: typical memory copy function
-             *
-             * (1) find src_kvaddr: the kernel virtual address of page
-             * (2) find dst_kvaddr: the kernel virtual address of npage
-             * (3) memory copy from src_kvaddr to dst_kvaddr, size is PGSIZE
-             * (4) build the map of phy addr of  nage with the linear addr start
-             */
+            /* LAB5:EXERCISE2 YOUR CODE */
 
+            // (1) find src_kvaddr: the kernel virtual address of page
+            void *src_kvaddr = page2kva(page);
+            // (2) find dst_kvaddr: the kernel virtual address of npage
+            void *dst_kvaddr = page2kva(npage);
+            // (3) memory copy from src_kvaddr to dst_kvaddr, size is PGSIZE
+            memcpy(dst_kvaddr, src_kvaddr, PGSIZE);
+            // (4) build the map of phy addr of  npage with the linear addr start
+            ret = page_insert(to, npage, start, perm);
+            
             assert(ret == 0);
         }
         start += PGSIZE;
